56d5d870ce081c93eb9a09c2cd0751d7d100ec4b
Jonas Stettner <mail@jstet.net>
2023-01-03 19:50:58 +0100

    db works now

--- db/init.sh
+++ db/init.sh
@@ -2,42 +2,32 @@
 set -e
 export PGPASSWORD=$POSTGRES_PASSWORD;
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"<<-EOSQL
-  CREATE TABLE y_codes(
-    y_code_id INT PRIMARY KEY);
-  
-  DO \$FN\$
-  BEGIN
-    FOR counter IN 1..49 LOOP
-      RAISE NOTICE 'Counter: %', counter;
-      EXECUTE format('ALTER TABLE y_codes ADD COLUMN Y%s BOOLEAN',counter) 
-        USING counter;
-    END LOOP;
-  END;
-  \$FN\$;
 
-  CREATE TABLE annex_3(
-    annex_3_id INT PRIMARY KEY,
-    "h1" BOOLEAN,
-    "h3" BOOLEAN,
-    "h4.1" BOOLEAN,
-    "h4.2" BOOLEAN,
-    "h4.3" BOOLEAN,
-    "h5.1" BOOLEAN,
-    "h5.2" BOOLEAN,
-    "h6.1" BOOLEAN,
-    "h6.2" BOOLEAN,
-    "h8" BOOLEAN,
-    "h10" BOOLEAN,
-    "h11" BOOLEAN,
-    "h12" BOOLEAN,
-    "h13" BOOLEAN);
-
-  CREATE TABLE annex_4_a(
-    annex_4_a_id INT PRIMARY KEY,
+ CREATE TABLE exports(
+    id INT PRIMARY KEY,
+    country VARCHAR(2),
+    country_of_destination VARCHAR(2),
+    year INT,  
+    amount FLOAT,
+    H1 BOOLEAN,
+    H3 BOOLEAN,
+    H4_1 BOOLEAN,
+    H4_2 BOOLEAN,
+    H4_3 BOOLEAN,
+    H5_1 BOOLEAN,
+    H5_2 BOOLEAN,
+    H6_1 BOOLEAN,
+    H6_2 BOOLEAN,
+    H8 BOOLEAN,
+    H10 BOOLEAN,
+    H11 BOOLEAN,
+    H12 BOOLEAN,
+    H13 BOOLEAN,
     D1 BOOLEAN,
     D2 BOOLEAN,
     D3 BOOLEAN,
     D4 BOOLEAN,
+    D5 BOOLEAN,
     D6 BOOLEAN,
     D7 BOOLEAN,
     D8 BOOLEAN,
@@ -47,14 +37,13 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"<<-EO
     D12 BOOLEAN,
     D13 BOOLEAN,
     D14 BOOLEAN,
-    D15 BOOLEAN);
-  
-  CREATE TABLE annex_4_b(
-    annex_4_b_id INT PRIMARY KEY,
+    D15 BOOLEAN,
+    D16 BOOLEAN,
     R1 BOOLEAN,
     R2 BOOLEAN,
     R3 BOOLEAN,
     R4 BOOLEAN,
+    R5 BOOLEAN,
     R6 BOOLEAN,
     R7 BOOLEAN,
     R8 BOOLEAN,
@@ -62,28 +51,11 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"<<-EO
     R10 BOOLEAN,
     R11 BOOLEAN,
     R12 BOOLEAN,
-    R13 BOOLEAN);
-  
-  CREATE TABLE exports(
-    id INT PRIMARY KEY,
-    annex_3_id INT,
-    annex_4_a_id INT,
-    annex_4_b_id INT,
-    amount INT,
-    country_of_destination VARCHAR(2),
-    country VARCHAR(100),
-    year INT,
-    CONSTRAINT fk_annex_3
-      FOREIGN KEY(annex_3_id) 
-	      REFERENCES annex_3(annex_3_id)
-    ,
-    CONSTRAINT fk_annex_4_a
-      FOREIGN KEY(annex_4_a_id) 
-	      REFERENCES annex_4_a(annex_4_a_id)
-    ,
-    CONSTRAINT fk_annex_4_b
-      FOREIGN KEY(annex_4_b_id) 
-	      REFERENCES annex_4_b(annex_4_b_id)
+    R13 BOOLEAN
     );
+  
+COPY exports(id, country, country_of_destination, year, amount,H1,H3,H4_1,H4_2,H4_3,H5_1,H5_2,H6_1,H6_2,H8,H10,H11,H12,H13,D1,D2,D3,D4,D5,D6,D7,D8,D9,D10,D11,D12,D13,D14,D15,D16,R1,R2,R3,R4,R5,R6,R7,R8,R9,R10,R11,R12,R13)
+FROM '/data/exports.csv'
+CSV HEADER DELIMITER ',';
   
 EOSQL
